cab16d2cf16849db568e4819b0ab6f52eea34a0f
rodrigo <barbaresco.rodrigo@gmail.com>
2018-06-06 10:15:29 -0300

    Adicionando mais testes; Transformando robot em classe.

--- robot.py
+++ robot.py
@@ -1,43 +1,45 @@
-def execute(*commands):
-    x = None
-    y = None
-    direction = None
-    placed = False
+class Robot:
 
-    for command in commands:
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.direction = None
+        self.placed = False
 
-        if "PLACE" in command:
-            placed = True
-            x, y, direction = command.strip("PLACE ").split(" ")
-        if "MOVE" in command:
-            if direction == "NORTH":
-                y = int(y) + 1
-            elif direction == "SOUTH":
-                y = int(y) - 1
-            elif direction == "EAST":
-                x = int(x) + 1
-            elif direction == "WEST":
-                x = int(x) - 1
-        if "LEFT" in command:
-            if direction == "NORTH":
-                direction = "WEST"
-            elif direction == "WEST":
-                direction = "SOUTH"
-            elif direction == "SOUTH":
-                direction = "EAST"
-            elif direction == "EAST":
-                direction = "NORTH"
-        if "RIGHT" in command:
-            if direction == "WEST":
-                direction = "NORTH"
-            elif direction == "NORTH":
-                direction = "EAST"
-            elif direction == "EAST":
-                direction = "SOUTH"
-    return build_return(x, y, direction, placed)
+    def execute(self, *commands):
+        for command in commands:
 
+            if "PLACE" in command:
+                self.placed = True
+                self.x, self.y, self.direction = command.strip("PLACE ").split(" ")
+            if "MOVE" in command:
+                if self.direction == "NORTH":
+                    self.y = int(self.y) + 1
+                elif self.direction == "SOUTH":
+                    self.y = int(self.y) - 1
+                elif self.direction == "EAST":
+                    self.x = int(self.x) + 1
+                elif self.direction == "WEST":
+                    self.x = int(self.x) - 1
+            if "LEFT" in command:
+                if self.direction == "NORTH":
+                    self.direction = "WEST"
+                elif self.direction == "WEST":
+                    self.direction = "SOUTH"
+                elif self.direction == "SOUTH":
+                    self.direction = "EAST"
+                elif self.direction == "EAST":
+                    self.direction = "NORTH"
+            if "RIGHT" in command:
+                if self.direction == "WEST":
+                    self.direction = "NORTH"
+                elif self.direction == "NORTH":
+                    self.direction = "EAST"
+                elif self.direction == "EAST":
+                    self.direction = "SOUTH"
+        return self.report()
 
-def build_return(x, y, direction, placed):
-    if not placed:
-        return "I'm not placed yet"
-    return ' '.join([str(x), str(y), direction])
+    def report(self):
+        if not self.placed:
+            return "I'm not placed yet"
+        return ' '.join([str(self.x), str(self.y), self.direction])
--- test.py
+++ test.py
@@ -1,60 +1,70 @@
 import unittest
+
 import robot
 
 
 class RobotTest(unittest.TestCase):
 
+    def setUp(self):
+        self.robot = robot.Robot()
+
     def test_deveria_receber_uma_lista_de_comandos(self):
-        robot.execute("SEi pa")
+        self.robot.execute("SEi pa")
 
     def test_deveria_receber_uma_lista_de_comandos_quando_tiver_dois(self):
-        robot.execute("SEi pa", "Outro comando")
+        self.robot.execute("SEi pa", "Outro comando")
 
     def test_deveria_retornar_not_placed_yet_quando_nao_tiver_sido_colocado(self):
-        self.assertEqual("I'm not placed yet", robot.execute("SEi pa", "Outro comando"))
+        self.assertEqual("I'm not placed yet", self.robot.execute("SEi pa", "Outro comando"))
 
     def test_deveria_retornar_0_0_NORTH(self):
-        self.assertEqual("0 0 NORTH", robot.execute("PLACE 0 0 NORTH"))
+        self.assertEqual("0 0 NORTH", self.robot.execute("PLACE 0 0 NORTH"))
 
     def test_deveria_retornar_0_1_NORTH(self):
-        self.assertEqual("0 1 NORTH", robot.execute("PLACE 0 0 NORTH", "MOVE"))
+        self.assertEqual("0 1 NORTH", self.robot.execute("PLACE 0 0 NORTH", "MOVE"))
 
     def test_deveria_retornar_0_0_WEST(self):
-        self.assertEqual("0 0 WEST", robot.execute("PLACE 0 0 NORTH", "LEFT"))
+        self.assertEqual("0 0 WEST", self.robot.execute("PLACE 0 0 NORTH", "LEFT"))
 
     def test_deveria_retornar_0_0_EAST(self):
-        self.assertEqual("0 0 EAST", robot.execute("PLACE 0 0 NORTH", "RIGHT"))
+        self.assertEqual("0 0 EAST", self.robot.execute("PLACE 0 0 NORTH", "RIGHT"))
 
     def test_deveria_retornar_0_0_NORTH_(self):
-        self.assertEqual("0 0 NORTH", robot.execute("PLACE 0 0 WEST", "RIGHT"))
+        self.assertEqual("0 0 NORTH", self.robot.execute("PLACE 0 0 WEST", "RIGHT"))
 
     def test_deveria_retornar_0_0_SOUTH(self):
-        self.assertEqual("0 0 SOUTH", robot.execute("PLACE 0 0 EAST", "RIGHT"))
+        self.assertEqual("0 0 SOUTH", self.robot.execute("PLACE 0 0 EAST", "RIGHT"))
 
     def test_deveria_retornar_0_0_SOUTH_passando_LEFT(self):
-        self.assertEqual("0 0 SOUTH", robot.execute("PLACE 0 0 WEST", "LEFT"))
+        self.assertEqual("0 0 SOUTH", self.robot.execute("PLACE 0 0 WEST", "LEFT"))
 
     def test_deveria_retornar_0_0_EAST_passando_LEFT(self):
-        self.assertEqual("0 0 EAST", robot.execute("PLACE 0 0 SOUTH", "LEFT"))
+        self.assertEqual("0 0 EAST", self.robot.execute("PLACE 0 0 SOUTH", "LEFT"))
 
     def test_deveria_retornar_0_0_NORTH_passando_LEFT(self):
-        self.assertEqual("0 0 NORTH", robot.execute("PLACE 0 0 EAST", "LEFT"))
+        self.assertEqual("0 0 NORTH", self.robot.execute("PLACE 0 0 EAST", "LEFT"))
 
     def test_deveria_retornar_0_0_NORTH_com_place_no_meio(self):
-        self.assertEqual("0 0 NORTH", robot.execute("PLACE 0 0 NORTH", "MOVE", "PLACE 0 0 NORTH"))
+        self.assertEqual("0 0 NORTH", self.robot.execute("PLACE 0 0 NORTH", "MOVE", "PLACE 0 0 NORTH"))
 
     def test_deveria_retornar_0_0_NORTH_comecando_de_0_1_SOUTH(self):
-        self.assertEqual("0 0 SOUTH", robot.execute("PLACE 0 1 SOUTH", "MOVE"))
+        self.assertEqual("0 0 SOUTH", self.robot.execute("PLACE 0 1 SOUTH", "MOVE"))
 
     def test_deveria_retornar_1_0_EAST_comecando_de_0_0_EAST(self):
-        self.assertEqual("1 0 EAST", robot.execute("PLACE 0 0 EAST", "MOVE"))
+        self.assertEqual("1 0 EAST", self.robot.execute("PLACE 0 0 EAST", "MOVE"))
 
     def test_deveria_retornar_1_0_WEST_comecando_de_2_0_WEST(self):
-        self.assertEqual("1 0 WEST", robot.execute("PLACE 2 0 WEST", "MOVE"))
+        self.assertEqual("1 0 WEST", self.robot.execute("PLACE 2 0 WEST", "MOVE"))
 
     def test_deveria_retornar_0_1_NORTH_com_place_no_meio_e_move(self):
-        self.assertEqual("0 1 NORTH", robot.execute("PLACE 0 0 NORTH", "MOVE", "PLACE 0 0 NORTH", "MOVE"))
+        self.assertEqual("0 1 NORTH", self.robot.execute("PLACE 0 0 NORTH", "MOVE", "PLACE 0 0 NORTH", "MOVE"))
 
-    def test_PLACE_nao_aceitando_negativo(self):
-        saldjlaksdj
+    def test_deveria_retornar_0_3_NORTH_com_3_moves(self):
+        self.assertEqual("0 3 NORTH", self.robot.execute("PLACE 0 0 NORTH", "MOVE", "MOVE", "MOVE"))
+
+    def test_deveria_retornar_0_3_EAST_com_3_moves_e_1_right(self):
+        self.assertEqual("0 3 EAST", self.robot.execute("PLACE 0 0 NORTH", "MOVE", "MOVE", "MOVE", "RIGHT"))
+
+    # def test_PLACE_nao_aceitando_negativo(self):
+    #     saldjlaksdj
 
